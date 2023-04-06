0c23c9a6c62b026bf3a7f0cdc59fd3a565ec7c05
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-06 21:20:22 +1000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,1 +1,16 @@
-magic
+
+<?php
+
+namespace skyss0fly\DynamicPockets;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+
+switch($command->getName()){
+if($sender->hasPermission("openpocket")){
+$inventory = $this->getServer()->createInventory($player, 9, "My Pocket");
+$player->addWindow($inventory);
+$inventory->setContents([item1, item2, item3]);
